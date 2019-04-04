a064d9af8d411ef9627496af48e9762b7f699604
Secrito45 <konsta.siekkeli@gmail.com>
2019-04-04 08:37:08 +0300

    First exercise explain the code

---
+++ explainCode.c
@@ -0,0 +1,51 @@
+/** 
+ * Task1:
+ * Comment the code where you see empthy "//" single line comments
+ * The command you need to explain is either next or underneath 
+ * the empthy single line comment.
+ * 
+ * You can write your answer to multible lines but all answers needs to be a comment
+ * 
+ * Use either Finnish or English when commenting the code
+ */
+
+
+//
+#include <stdio.h>
+
+//
+int main()
+{
+    //
+    int amount = 5;
+
+    //
+    //
+    //
+    for(int i = 0; i < amount; i++)
+    {
+        printf("Hello, World! %d", i);
+    }
+
+    //
+    while(1)
+    {
+
+    }
+
+    return 0;
+}
+
+/**
+ * Taks2:
+ * Explain the flow of the code.
+ * 
+ * -
+ */
+
+/**
+ * Task3:
+ * Explain the purpuse of %d in printf() func call
+ * 
+ * -
+ */
