50d1ac2fddc40d5a9e79fbc330d010fe5143a7b5
Helen Ronkainen <helenquelli@hotmail.com>
2019-04-07 10:21:13 +0300

    Update explainCode.c

--- explainCode.c
+++ explainCode.c
@@ -13,21 +13,21 @@
 //
 #include <stdio.h>
 
-//
+//main function, with parameters added and array
 int main(int argc, char const *argv[])
 {
-    //
+    // short of integer variable declaration
     int amount = 5;
 
     //
     //
-    //
+    // this is a loop with tenary operation 
     for(int i = 0; i < amount; i++)
     {
         printf("Hello, World! %d \n", i);
     }
 
-    //
+    // this is a while loop
     while(1){
 
     }
@@ -37,15 +37,16 @@ int main(int argc, char const *argv[])
 /**
  * Task2:
  * Dose the code run ever reach return, if no explain why that is so 
- * 
- * -
+ * it doesnt run 
+ * - yes it does run and prints hello word from 0 to 5 
  */
 
 /**
  * Taks3:
  * Explain the flow of the code.
  * 
- * -
+ * - an variable is declared, and the amount is 5.
+ then is added a tenary operation inside the for loop which gives comand to print it 5 times. 
  */
 
 /**
